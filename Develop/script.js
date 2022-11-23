c125f9a27fe89f90510e1318794d6cb75434e266
Salena O'Neill <salenaoneill@salenas-air.lan>
2022-11-23 15:10:32 -0600

    added color-coded time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,46 @@
+var date_today = dayjs().format('dddd, MMMM, D');
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+    $('.saveBtn').on("click", function() {
+        //save text in local storage
+    })
+
+    function timeblock_color_set(){
+        var current_hour = dayjs().hour()
+        $('.time-block').each(function(){
+            var block_hour = parseInt(this.id.replace("hour-", ""));
+            if (current_hour > block_hour){
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+    
+            else if (current_hour === block_hour){
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+    
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    
+       
+    };
+    timeblock_color_set();
+    
+    
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
