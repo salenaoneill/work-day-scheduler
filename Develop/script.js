8e62ba410c9a6e7bd372a2f88655293b9e449665
Salena O'Neill <salenaoneill@salenas-air.lan>
2022-11-27 18:40:42 -0600

    comments, read-me, current date

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,11 @@
-var date_today = dayjs().format('dddd, MMMM, D');
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//display todays date
+var date_today = dayjs().format('dddd, MMMM D');
+$("#currentDay").text(date_today);
+
 $(function () {
+    //when the save button is clicked, 
+    //save the timeblock as the key in local storage, 
+    //& save the text entered by the user in the timeblock as the value in local storage.
     $('.saveBtn').on("click", function() {
         var schedule_text = $(this).siblings(".description").val();
         var schedule_hour = $(this).parent().attr("id");
@@ -15,18 +18,22 @@ $(function () {
         var current_hour = dayjs().hour()
         $('.time-block').each(function(){
             var block_hour = parseInt(this.id.replace("hour-", ""));
+            //if the timeblock is in a past hour, add a class of "past"
+            //which will then make the background gray
             if (current_hour > block_hour){
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-    
+            //if the timeblock is in the present hour, add a class of "present"
+            //which will then make the background red
             else if (current_hour === block_hour){
                 $(this).removeClass("future");
                 $(this).removeClass("past");
                 $(this).addClass("present");
             }
-    
+            //if the timeblock is in a future hour, add a class of "future"
+            //which will then make the background green
             else {
                 $(this).removeClass("past");
                 $(this).removeClass("present");
@@ -38,28 +45,13 @@ $(function () {
     };
     timeblock_color_set();
     
+    //retrieve the user input typed into a textbox from local storage
+    //& display it on the time-block representing that hour
     $(".time-block .description").each(function(){
         var schedule_hour = $(this).parent().attr("id"); 
         var this_hour = localStorage.getItem(schedule_hour);
         $(this).text(this_hour);
     });
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
   });
   
   
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# work-day-scheduler
+# <Work-Day-Scheduler>
+
+## Description
+
+My motivation for this project was to practice using jquery and utilize local storage to build a daily planner that allows a user to save events on an hourly basis.
+
+## Usage
+
+All the user has to do to use this webpage is type their hourly events into the text box next to the hour in which they will be attending/completing the event. Then, click the blue save button to the right of the text box to save the event
+
+![application image](Assets/application.png)
+
+## License
+
+MIT License
+
+## Deployed Link
